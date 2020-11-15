cd0042da380fef732abfe93b006ede1dfc730644
Stephen Planck <splanck9271@gmail.com>
2020-11-16 03:56:25 +1000

    Added a few comments.

--- kernel.cpp
+++ kernel.cpp
@@ -1,13 +1,17 @@
+// kernel.cpp - Main kernel source file.
+
+void printf(char* str); 
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
+    printf("This is a test kernel!");
+
+    while(1);
+}
+
 void printf(char* str) {
     unsigned short* VideoMemory = (unsigned short*)0xb8000;
 
     for(int i = 0; str[i] != '\0'; i++) {
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
     }
-}
-
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber) {
-    printf("This is a test kernel!");
-
-    while(1);
 }
