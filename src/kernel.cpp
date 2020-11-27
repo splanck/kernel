deb89f7fcd2071364796371ecbdf4fb4ac8dd49b
Stephen Planck <splanck9271@gmail.com>
2020-11-27 21:01:31 +1000

    Improved the kprint function to handle line feeds.

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -6,7 +6,8 @@
 // Execution starting point for the kernel.
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
-    kprint("This is a test kernel! OH OK!");
+    kprint("This is a test kernel! OH OK!\n");
+    kprint("This is another test!");
 
     GlobalDescriptorTable gdt;
 
@@ -25,10 +26,45 @@ extern "C" void callConstructors()
 // Basic text print function.
 void kprint(char* s) 
 {
-    uint16_t* VideoMemory = (uint16_t*)0xb8000;
+    uint16_t* vmem = (uint16_t*)0xb8000;
 
-    for(int x = 0; s[x] != '\0'; x++) 
+    static uint8_t x = 0, y = 0;
+    uint8_t c = 80 * y + x;
+
+    for(int i = 0; s[i] != '\0'; i++) 
     {
-        VideoMemory[x] = (VideoMemory[x] & 0xFF00) | s[x];
+        if(s[i] == '\n')
+        {
+            y++;
+            x = 0;
+        }
+        else
+        {
+            c = 80 * y + x;
+            vmem[c] = (vmem[c] & 0xFF00) | s[i];
+
+            x++;
+        }
+
+        if(x >= 80) 
+        {
+            y++;
+            x = 0;
+        }
+
+        if(y >= 25) 
+        {
+            for(y = 0; y < 25; y++)
+            {
+                for(x = 0; x < 80; x++)
+                {
+                    c = 80 * y + x;
+                    vmem[c] = (vmem[c] & 0xFF00) | ' ';
+                }
+            }
+
+            x = 0;
+            y = 0;
+        }
     }
 }
